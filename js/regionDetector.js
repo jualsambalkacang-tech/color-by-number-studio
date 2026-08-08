ccd63472c327c3d1a340401e75b5b78e6a69343d
jualsambalkacang-tech <jualsambalkacang@gmail.com>
2026-08-08 09:29:48 +0700

    Create regionDetector.js

---
+++ js/regionDetector.js
@@ -0,0 +1,21 @@
+class RegionDetector {
+
+    constructor(imageData){
+
+        this.imageData = imageData;
+        this.width = imageData.width;
+        this.height = imageData.height;
+
+        this.regions = [];
+
+    }
+
+    detect(){
+
+        console.log("Region detection dimulai...");
+
+        return this.regions;
+
+    }
+
+}
